970ecb07b977ec1e8f25214907078d68207a48ed
王章权 <2653015738@qq.com>
2021-10-12 22:27:00 +0800

    测试

---
+++ test_10_9/test.c
@@ -0,0 +1,27 @@
+
+ 
+
+#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 18;
+//	double weight = 55.5;
+//
+//		age = age + 1;
+//		weight = weight + 1;
+//		printf("%lf\n", weight);
+//		printf("%d\n", age);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
+	return 0;
+}
